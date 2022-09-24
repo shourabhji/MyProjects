b56a5289a49a8664c9043e5e343a33cee6dc9c00
Shourabh <shourabhpandey28@gmail.com>
2022-09-24 12:34:12 +0530

    this app is compleated

--- MyProject/server/index.js
+++ MyProject/server/index.js
@@ -5,12 +5,10 @@ const dotenv = require("dotenv");
 const PORT = process.env.PORT || 5000 ;
 const app = express();
 dotenv.config();
-
-
 app.use(express.json({extendex:true}) , express.urlencoded({extended:true}));
 app.use(cors());
 
-const mongoURI = "mongodb+srv://shourabhji:kam12345@cluster0.wajhdup.mongodb.net/MyProjects?retryWrites=true&w=majority";
+const mongoURI = process.env.MONGO;
 mongoose.connect(mongoURI , {useUnifiedTopology: true}).then(()=>{console.log("connect to mmongodb")}).catch(err => console.log(err));
 
 
@@ -20,7 +18,7 @@ app.get('/' , (req,res) =>{
 })
 app.use('/api/auth' , require('./routes/auth.js'));
 app.use('/api/projects' , require('./routes/projects.js'));
-
+ 
 app.listen(PORT , ()=> {
     console.log(`your app is running on http://localhost:${PORT}`)
 });
